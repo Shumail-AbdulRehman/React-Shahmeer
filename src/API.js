c2b9a01a89385a3505f472dbd4a2c8aab64dacd4
Shumail-AbdulRehman <47891@students.riphah.edu.pk>
2025-01-13 02:15:21 +0500

    Updated backend URL to Azure

--- src/API.js
+++ src/API.js
@@ -1,4 +1,4 @@
 // const URI = 'http://localhost:8080';
-const URI = 'https://rogue-mewing-krill.glitch.me';
-export const BASE_URL = URI ;
+const URI = 'https://shahmeer-backend-ehgngbczg8cfbpa9.uksouth-01.azurewebsites.net';
+export const BASE_URL = URI;
 export const AUTH = `${BASE_URL}/auth`;
