866b1b57a489d70a0d00222db1e0b4ebbd106282
SHUMAIL <47891@students.riphah.edu.pk>
2025-01-13 14:03:53 +0500

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,4 +1,6 @@
-const URI = 'http://localhost:8080';
+// const URI = 'http://localhost:8080';
 // const URI = 'https://shahmeer-backend-ehgngbczg8cfbpa9.uksouth-01.azurewebsites.net/';
+const URI = 'https://rogue-mewing-krill.glitch.me';
+
 export const BASE_URL = URI;
 export const AUTH = `${BASE_URL}/auth`;
