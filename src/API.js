cdc92fb41ee9458c6c8034333dbb43492e88617d
Shumail-AbdulRehman <47891@students.riphah.edu.pk>
2025-01-13 10:01:52 +0500

    he

--- src/API.js
+++ src/API.js
@@ -1,4 +1,4 @@
 // const URI = 'http://localhost:8080';
-const URI = 'https://shahmeer-backend-ehgngbczg8cfbpa9.uksouth-01.azurewebsites.net';
+const URI = 'https://shahmeer-backend-ehgngbczg8cfbpa9.uksouth-01.azurewebsites.net/';
 export const BASE_URL = URI;
 export const AUTH = `${BASE_URL}/auth`;
